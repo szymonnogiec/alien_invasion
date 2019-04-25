954c4342e19d5cd503375ed2c5f32968dbe49314
Szymon Nogieć <szymon.nogiec@gmail.com>
2019-04-25 19:59:54 +0200

    Add first line of alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,9 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
 import game_functions as gf
-from pygame.sprite import Group
 
 
 def run_game():
@@ -13,12 +13,14 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 
 def check_events(ai_settings, screen, ship, bullets):
@@ -14,13 +15,13 @@ def check_events(ai_settings, screen, ship, bullets):
             check_key_up_event(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Update images on the screen and flkp to a new screen"""
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
-
     ship.blit_me()
+    aliens.draw(screen)
     pygame.display.flip()
 
 
@@ -32,6 +33,8 @@ def check_key_down_event(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 
 def check_key_up_event(event, ship):
@@ -55,3 +58,19 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+
+
+def create_fleet(ai_settings, screen, aliens):
+    """Create full fleet of alients"""
+    # Create alien and find the number of aliens in a row
+    # Spacing between each alien is equal to one alien width
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    for alien_number in range(number_aliens_x):
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
